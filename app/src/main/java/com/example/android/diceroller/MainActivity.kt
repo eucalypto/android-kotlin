57bb544b5a45d4887cde51fc986381a9f9b864ea
Viktor Papara <eucalyptov@gmail.com>
2020-03-17 14:12:15 +0100

    Add method to roll pseudo random numbers at button press

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.diceroller
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        roll_button.text = "Let's Roll!"
+        roll_button.text = getString(R.string.roll_button_text)
+        roll_button.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        result_text.text = randomInt.toString()
     }
 }
