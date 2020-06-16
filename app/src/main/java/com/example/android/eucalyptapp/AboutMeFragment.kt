5274e723dbc6320453338197fac01248b3834974
Viktor Papara <eucalypto@mail.de>
2020-06-16 23:16:32 +0200

    Add nickname input in AboutMe Fragment
    According to concept 12 - 14 from Lesson2: Layouts

--- app/src/main/java/com/example/android/eucalyptapp/AboutMeFragment.kt
+++ app/src/main/java/com/example/android/eucalyptapp/AboutMeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.android.eucalyptapp
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_about_me.*
 
 class AboutMeFragment : Fragment() {
 
@@ -14,5 +17,22 @@ class AboutMeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about_me, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        doneButton.setOnClickListener { addNickname() }
+    }
+
+    private fun addNickname() {
+        nicknameText.text = nicknameEdit.text
+        nicknameEdit.visibility = View.GONE
+        doneButton.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+
+        // Hide keyboard
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(doneButton.windowToken, 0)
     }
 }
