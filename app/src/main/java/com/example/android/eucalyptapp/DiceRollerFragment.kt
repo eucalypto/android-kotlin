f1f53d875794fc03c9667633b3459eadd99bc017
Viktor Papara <eucalypto@mail.de>
2020-06-16 16:35:22 +0200

    Move dice roller to separate fragment
    and update gradle dependencies and set up ktlint as kotlin linter

---
+++ app/src/main/java/com/example/android/eucalyptapp/DiceRollerFragment.kt
@@ -0,0 +1,42 @@
+package com.example.android.eucalyptapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_dice_roller.*
+import java.util.Random
+
+
+class DiceRollerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dice_roller, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rollButton.text = getString(R.string.roll_button_text)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val diceResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(diceResource)
+    }
+}
--- app/src/main/java/com/example/android/eucalyptapp/MainActivity.kt
+++ app/src/main/java/com/example/android/eucalyptapp/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.android.eucalyptapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,21 +9,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        roll_button.text = getString(R.string.roll_button_text)
-        roll_button.setOnClickListener {
-            rollDice()
-        }
-    }
-
-    private fun rollDice() {
-        val diceResource = when (Random().nextInt(6) + 1) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        dice_image.setImageResource(diceResource)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.mainActivity, DiceRollerFragment())
+            .commit()
     }
 }
