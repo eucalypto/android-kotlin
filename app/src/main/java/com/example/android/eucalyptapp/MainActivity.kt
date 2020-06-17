9c6527de5d5789ea37d04664ac5ee9f20aaba8af
Viktor Papara <eucalypto@mail.de>
2020-06-17 12:32:45 +0200

    [#5] copy about-me code from fragment into main activity
    This produces correct behavior.

--- app/src/main/java/com/example/android/eucalyptapp/MainActivity.kt
+++ app/src/main/java/com/example/android/eucalyptapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.android.eucalyptapp
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_about_me.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager
-            .beginTransaction()
-            // .add(R.id.mainActivity, DiceRollerFragment())
-            .add(R.id.mainActivity, AboutMeFragment())
-            .commit()
+        doneButton.setOnClickListener { addNickname() }
+    }
+
+    private fun addNickname() {
+        nicknameText.text = nicknameEdit.text
+        nicknameEdit.visibility = View.GONE
+        doneButton.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+
+        // Hide keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(doneButton.windowToken, 0)
     }
 }
