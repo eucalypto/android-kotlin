f2aa9e824cc4481a585107a91c6ad2c9a7825790
Viktor Papara <eucalypto@mail.de>
2020-06-19 14:28:58 +0200

    Use ViewBinding in AboutMeFragment instead of Kotlin synthetics

--- app/src/main/java/com/example/android/eucalyptapp/AboutMeFragment.kt
+++ app/src/main/java/com/example/android/eucalyptapp/AboutMeFragment.kt
@@ -7,9 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_about_me.*
+import com.example.android.eucalyptapp.databinding.FragmentAboutMeBinding
 
 class AboutMeFragment : Fragment() {
+
+    private var _fragmentAboutMeBinding: FragmentAboutMeBinding? = null
+    private val fragmentAboutMeBinding: FragmentAboutMeBinding
+        get() {
+            return _fragmentAboutMeBinding ?: throw NullPointerException(
+                "View binding only allowed in Fragment's view lifecycle: between onCreateView and onDestroyView"
+            )
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,18 +29,24 @@ class AboutMeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _fragmentAboutMeBinding = FragmentAboutMeBinding.bind(view)
 
-        doneButton.setOnClickListener { addNickname() }
+        fragmentAboutMeBinding.doneButton.setOnClickListener { addNickname() }
     }
 
     private fun addNickname() {
-        nicknameText.text = nicknameEdit.text
-        nicknameEdit.visibility = View.GONE
-        doneButton.visibility = View.GONE
-        nicknameText.visibility = View.VISIBLE
+        fragmentAboutMeBinding.nicknameText.text = fragmentAboutMeBinding.nicknameEdit.text
+        fragmentAboutMeBinding.nicknameEdit.visibility = View.GONE
+        fragmentAboutMeBinding.doneButton.visibility = View.GONE
+        fragmentAboutMeBinding.nicknameText.visibility = View.VISIBLE
 
         // Hide keyboard
         val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(doneButton.windowToken, 0)
+        imm.hideSoftInputFromWindow(fragmentAboutMeBinding.doneButton.windowToken, 0)
+    }
+
+    override fun onDestroyView() {
+        _fragmentAboutMeBinding = null
+        super.onDestroyView()
     }
 }
