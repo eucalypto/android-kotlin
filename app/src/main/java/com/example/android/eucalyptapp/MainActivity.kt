ea9e6c6d23ab67692b74bba3d7cb4abe7fd264f2
Viktor Papara <eucalypto@mail.de>
2020-07-05 21:16:18 +0200

    Implement navigation drawer to switch between course project fragments
    Take inspiration from the Android Studio Template:
    https://developer.android.com/studio/projects/templates#NavigationDrawer
    (#8, #7)

--- app/src/main/java/com/example/android/eucalyptapp/MainActivity.kt
+++ app/src/main/java/com/example/android/eucalyptapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.android.eucalyptapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.android.eucalyptapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.navView.setupWithNavController(navController)
     }
 }
