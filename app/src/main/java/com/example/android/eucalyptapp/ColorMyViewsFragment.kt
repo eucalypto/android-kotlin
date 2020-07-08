a7bde1f99a73f15a60c62289a9120221b908092b
Viktor Papara <eucalypto@mail.de>
2020-07-08 18:59:57 +0200

    Add clickListeners that change the color of the boxes when clicked (#7)

--- app/src/main/java/com/example/android/eucalyptapp/ColorMyViewsFragment.kt
+++ app/src/main/java/com/example/android/eucalyptapp/ColorMyViewsFragment.kt
@@ -1,12 +1,22 @@
 package com.example.android.eucalyptapp
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.android.eucalyptapp.databinding.FragmentColorMyViewsBinding
 
 class ColorMyViewsFragment : Fragment() {
+
+    private var _binding: FragmentColorMyViewsBinding? = null
+    private val binding: FragmentColorMyViewsBinding
+        get() {
+            return _binding ?: throw NullPointerException(
+                "View binding only allowed in Fragment's view lifecycle: between onCreateView and onDestroyView"
+            )
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,5 +24,41 @@ class ColorMyViewsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_color_my_views, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentColorMyViewsBinding.bind(view)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(
+            binding.textView1,
+            binding.textView2,
+            binding.textView3,
+            binding.textView4,
+            binding.textView5,
+            binding.colorMyViewsLayout
+        )
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            R.id.textView1 -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.textView2 -> view.setBackgroundColor(Color.GRAY)
+
+            R.id.textView3 -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.textView4 -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.textView5 -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
