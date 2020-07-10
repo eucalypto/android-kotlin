8748928516180a586ba477fbfafd51d626f0f43f
Viktor Papara <eucalypto@mail.de>
2020-07-10 13:37:25 +0200

    Set-up action bar with up button (#9)

--- app/src/main/java/de/eucalypto/eucalyptapp/MainActivity.kt
+++ app/src/main/java/de/eucalypto/eucalyptapp/MainActivity.kt
@@ -3,6 +3,7 @@ package de.eucalypto.eucalyptapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import de.eucalypto.eucalyptapp.databinding.ActivityMainBinding
 
@@ -16,5 +17,10 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment)
         binding.navView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return this.findNavController(R.id.nav_host_fragment).navigateUp()
     }
 }
