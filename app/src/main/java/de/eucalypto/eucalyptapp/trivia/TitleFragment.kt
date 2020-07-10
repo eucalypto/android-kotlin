5b16c31f6bcdc18b6868fdb3ae35af22b86475b5
Viktor Papara <eucalypto@mail.de>
2020-07-10 14:00:19 +0200

    Set-up overflow menu for game fragment (#9)

--- app/src/main/java/de/eucalypto/eucalyptapp/trivia/TitleFragment.kt
+++ app/src/main/java/de/eucalypto/eucalyptapp/trivia/TitleFragment.kt
@@ -2,10 +2,15 @@ package de.eucalypto.eucalyptapp.trivia
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import de.eucalypto.eucalyptapp.R
 import de.eucalypto.eucalyptapp.databinding.FragmentTitleBinding
 
@@ -25,6 +30,7 @@ class TitleFragment : Fragment() {
         // Inflate the layout for this fragment
         // return inflater.inflate(R.layout.fragment_title, container, false)
         _binding = FragmentTitleBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -34,6 +40,16 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, findNavController(this))
+            || super.onOptionsItemSelected(item)
     }
 
     override fun onDestroyView() {
