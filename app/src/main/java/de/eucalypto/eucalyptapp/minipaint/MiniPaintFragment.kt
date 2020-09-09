c7b65cb4cb85af08c9f00032755226d078f5e217
Viktor Papara <eucalypto@mail.de>
2020-09-09 14:34:11 +0200

    Create MyCanvasView and inflate it the way as before (#20)

--- app/src/main/java/de/eucalypto/eucalyptapp/minipaint/MiniPaintFragment.kt
+++ app/src/main/java/de/eucalypto/eucalyptapp/minipaint/MiniPaintFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import de.eucalypto.eucalyptapp.databinding.FragmentMiniPaintBinding
 
@@ -16,6 +17,13 @@ class MiniPaintFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding = FragmentMiniPaintBinding.inflate(inflater, container, false)
 
+        (activity as AppCompatActivity).supportActionBar?.hide()
+
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        (activity as AppCompatActivity).supportActionBar?.show()
     }
 }
---
+++ app/src/main/java/de/eucalypto/eucalyptapp/minipaint/MyCanvasView.kt
@@ -0,0 +1,12 @@
+package de.eucalypto.eucalyptapp.minipaint
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class MyCanvasView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+}
