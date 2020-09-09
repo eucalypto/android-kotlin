8e9b06e13112da813fd5b7032346960aa93edfbb
Viktor Papara <eucalypto@mail.de>
2020-09-10 00:15:22 +0200

    Finish MyCanvasView (#20)
    It now waits for touch events and draws the corresponding line on the
    screen.

--- app/src/main/java/de/eucalypto/eucalyptapp/minipaint/MyCanvasView.kt
+++ app/src/main/java/de/eucalypto/eucalyptapp/minipaint/MyCanvasView.kt
@@ -3,9 +3,17 @@ package de.eucalypto.eucalyptapp.minipaint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Rect
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
 import de.eucalypto.eucalyptapp.R
+import kotlin.math.abs
+
+private const val STROKE_WIDTH = 12f
 
 class MyCanvasView(
     context: Context
@@ -16,16 +24,98 @@ class MyCanvasView(
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
 
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    // Set up the paint with which to draw
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device ar down-sampled
+        isDither = true
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+    }
+
+    private var path = Path()
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    private lateinit var frame: Rect
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         if (::extraBitmap.isInitialized) extraBitmap.recycle()
         extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(extraBitmap)
         extraCanvas.drawColor(backgroundColor)
+
+        // Rectangular frame around picture
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+        canvas.drawRect(frame, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUP()
+        }
+
+        return true
+    }
+
+    private var currentX = 0f
+
+    private var currentY = 0f
+
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    private fun touchMove() {
+        val dx = abs(motionTouchEventX - currentX)
+        val dy = abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            path.quadTo(
+                currentX,
+                currentY,
+                (motionTouchEventX + currentX) / 2,
+                (motionTouchEventY + currentY) / 2
+            )
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
+    }
+
+    private fun touchUP() {
+        path.quadTo(
+            (motionTouchEventX + currentX) / 2,
+            (motionTouchEventY + currentY) / 2,
+            motionTouchEventX,
+            motionTouchEventY
+        )
+        extraCanvas.drawPath(path, paint)
+        invalidate()
     }
 }
